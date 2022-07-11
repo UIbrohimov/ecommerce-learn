a95944a7568f904c98dc40a1cba5f4fd23c4bb30
Ubaydullo <ubuhobbit@gmail.com>
2022-07-11 23:07:06 +0500

    Set up staticfiles, add carousel to the home page

--- ecomLearn/urls.py
+++ ecomLearn/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +8,4 @@ from utils.homeview import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
