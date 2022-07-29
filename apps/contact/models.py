50c220ea3d2e70966aba3cbd05608036878a7969
adulaziziy <adulaziziy@gmail.com>
2022-07-29 18:24:19 +0500

    update .gitignore

--- apps/contact/models.py
+++ apps/contact/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Contact(models.Model):
     email = models.EmailField(max_length=255, blank=True)
     email2 = models.EmailField(max_length=255, blank=True)
@@ -16,7 +13,7 @@ class Contact(models.Model):
 from django.db import models
 
 
-class ContactView(models.Model):
+class Message(models.Model):
     name = models.TextField()
     phone = models.CharField(max_length=255)
     message = models.TextField()
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render
 from .forms import ContactForm
 
 from .models import Contact
-# Create your views here.
-
-
-
 
 def contact_view(request):
     ctx = Contact.objects.all()
-    return render(request, 'contact/contact.html', {'ctx': ctx})
-
-
-
-def contactView(request):
-    form = ContactForm()
-    context = {'form': form}
+    form = ContactForm
+    context = {'form': form, "ctx": ctx}
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+        else:
+            form = ContactForm(data=request.POST)
+            context["form"] = form
     return render(request, 'contact/contact.html', context)
+
+
