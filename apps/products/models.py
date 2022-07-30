24f30ef061ecde4daa91612557b70950456bd4ba
Ubaydullo <ubuhobbit@gmail.com>
2022-07-30 20:52:59 +0500

    Fixing foregin key relattion from prod to cat

--- apps/products/models.py
+++ apps/products/models.py
@@ -3,7 +3,14 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 class Result(models.Model):
   title = models.CharField('Product',max_length=255)
-  category = TreeForeignKey('Group',null=True,blank=True,on_delete=models.CASCADE)
+  # category = TreeForeignKey('Group',null=True,blank=True,on_delete=models.CASCADE)
+  cat = models.ForeignKey(
+    'Group',
+    on_delete=models.SET_NULL,
+    blank=True,
+    null=True,
+    related_name="products"
+  )
   price = models.CharField('Price', max_length=255)
   content = models.TextField('Information')
   image = models.ImageField('Image')
