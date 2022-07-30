347e5a114232322b3c998170c6051e810a8b82c1
Ubaydullo <ubuhobbit@gmail.com>
2022-07-30 20:51:03 +0500

    Fixing MPTT model

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Result, Group
-from mptt.admin import MPTTModelAdmin
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
 # Register your models here.
 
 admin.site.register(Result)
-admin.site.register(Group, MPTTModelAdmin) 
+
+@admin.register(Group)
+class GroupAdmin(DraggableMPTTAdmin):
+    # custom options what you want
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"slug": ('name',)}
--- apps/products/models.py
+++ apps/products/models.py
@@ -24,27 +24,14 @@ class Result(models.Model):
 class Group(MPTTModel):
   name = models.CharField(max_length=255, unique=True)
   parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True, on_delete=models.CASCADE)
-  slug = models.SlugField(max_length=255, default=name)
+  slug = models.SlugField(max_length=255, unique=True)
 
   class MPTTMeta:
     order_insertion_by = ['name']
 
   class Meta:
-    unique_together = (('parent', 'slug',))
     verbose_name = 'category'
     verbose_name_plural = 'categories'
-
-  def get_slug_list(self):
-    try:
-      ancestors = self.get_ancestors(include_self=True)
-    except:
-      ancestors = []
-    else:
-      ancestors = [ i.slug for i in ancestors]
-    slugs = []
-    for i in range(len(ancestors)):
-      slugs.append('/'.join(ancestors[:i+1]))
-    return slugs
 
   def __str__(self):
     return self.name
