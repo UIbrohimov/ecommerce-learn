5a6b98462c5d4daf9956efd7b9ff61e6242a50d4
bekzod0525 <bekzodshermatov2005@gmail.com>
2022-08-01 19:00:55 +0500

    gallery tayyor bootstarp yaxshi emas

--- apps/extra/urls.py
+++ apps/extra/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 
-from .views import my_view, MyView, SalomBolalar, gallery_view
+from .views import gallery_view
 
 
 app_name = "extra"
 urlpatterns = [
-    path('', my_view, name="my_view"),
-    path('class-based/', MyView.as_view(), name="another_view"),
-    path('class-based1/', SalomBolalar.as_view(), name="another_view1"),
     path("gallery/", gallery_view, name="gallery")
 ]
