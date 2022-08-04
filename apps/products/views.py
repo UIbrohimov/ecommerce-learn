d28f63f53b04a2050a40c44d86279182754d19f9
l3ekzod <l3ekzod07@gmail.com>
2022-08-04 20:52:19 +0500

    product template qo'shildi

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 
-def products_view(request):
-    return render(request, 'products/products.html')
+def product_detail(request):
+    return render(request, 'products/product-detail.html')
+
+def products_list(request):
+    products = Product.objects.all()
+    category = Category.objects.all()
+    return render(request, 'products/products.html', {'products': products, 'category': category })
